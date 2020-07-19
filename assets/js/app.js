5716df4fd080366501c0187f97ba843c92586d19
Cristhian <green2cerberuz@gmail.com>
2020-07-19 09:35:27 -0500

    (EN-1) feat: add basic html layout, buttons and custom css for easy notes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,21 @@
 console.clear();
 console.log("First Test");
+
+// Add basic open modal functionality
+function openModal(event){
+  console.log('Enter the button');
+  let modal = document.querySelector('.modal');
+  modal.classList.toggle('is-active');
+}
+
+// Close modal functionality, improve just for testing html modal code
+function closeModal(event){
+  let modal = document.querySelector('.modal');
+  modal.classList.toggle('is-active');
+}
+
+let addNote = document.querySelector('.add-note');
+addNote.addEventListener('click', openModal);
+
+let closeNote = document.querySelector('.delete');
+closeNote.addEventListener('click', closeModal);
