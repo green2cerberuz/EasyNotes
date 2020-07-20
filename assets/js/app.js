90a305fa35c921eb9b9e1eb2fc7a56e6756249ed
Cristhian <green2cerberuz@gmail.com>
2020-07-19 20:57:08 -0500

    (EN-8) feat: add support to add note dinamically inside app

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,6 @@ console.log("First Test");
 
 // Add basic open modal functionality
 function openModal(event){
-  console.log('Enter the button');
   let modal = document.querySelector('.modal');
   modal.classList.toggle('is-active');
 }
@@ -14,8 +13,80 @@ function closeModal(event){
   modal.classList.toggle('is-active');
 }
 
+function initNotesStack(){
+  notes = [];
+}
+
+function saveNote(event){
+  let modal_body = document.querySelector('.modal-card-body');
+  input = modal_body.getElementsByTagName('textarea')[0];
+  if(localStorage.getItem('notes')){
+    notes = JSON.parse(localStorage.getItem('notes'));
+  }else{
+    notes = [];
+  }
+  notes.push(input.value);
+  localStorage.setItem('notes', JSON.stringify(notes));
+  let note = createNote(input.value);
+  document.querySelector('.notes-stack').appendChild(note);
+  modal_body.parentElement.parentElement.classList.toggle('is-active');
+  input.value= '';
+
+}
+
+
+
 let addNote = document.querySelector('.add-note');
 addNote.addEventListener('click', openModal);
 
-let closeNote = document.querySelector('.delete');
+// add later a generic handler to add close function to modal-close events
+let closeNote = document.querySelector('.close-modal');
 closeNote.addEventListener('click', closeModal);
+
+let note = document.querySelector('#save-note');
+console.log(note);
+note.addEventListener('click', saveNote);
+
+
+/** Create note utilities for note objects */
+function createNoteBody(){
+  /**  Create note body to create card data dinamically*/
+  let column_wraper, column, note_date, note_title, note_brief;
+  column_wraper = document.createElement('div');
+  column_wraper.classList.add('columns');
+
+  // Internal column
+  column = document.createElement('div');
+  column.classList.add('column');
+  column.classList.add('is-12');
+
+  // elements of a note
+  note_date = document.createElement('p');
+  note_date.classList.add('note-date');
+
+  note_title = document.createElement('p');
+  note_title.classList.add('note-title');
+
+  note_brief = document.createElement('p');
+  note_brief.classList.add('note-brief');
+
+  column.appendChild(note_date);
+  column.appendChild(note_title);
+  column.appendChild(note_brief);
+
+  column_wraper.appendChild(column);
+  return column_wraper;
+}
+
+function createNote(noteValue){
+  /** Create a note and rrender on note stack*/
+  let noteBody = createNoteBody()
+  let note = document.createElement('div');
+  note.classList.add('note');
+  note.appendChild(noteBody);
+  note.querySelector('.note-title').textContent = noteValue;
+  note.querySelector('.note-date').textContent = new Date();
+  note.querySelector('.note-brief').textContent = noteValue
+  return note;
+
+}
