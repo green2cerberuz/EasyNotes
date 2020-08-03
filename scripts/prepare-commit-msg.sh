4f4f29b5bcff9f174c5e4f70a85df1162e222f66
Cristhian <green2cerberuz@gmail.com>
2020-08-03 08:33:58 -0500

    (EN-15) feat: add script file for precommit and references

--- scripts/prepare-commit-msg.sh
+++ scripts/prepare-commit-msg.sh
@@ -1,3 +1,12 @@
-NAME=$(git branch | grep '*' | sed 's/* //')
-echo "($NAME)": "$(cat $1)" > $1
+#!/bin/bash
+# Script taken from:
+# https://stackoverflow.com/questions/5894946/how-to-add-gits-branch-name-to-the-commit-message
+# and modified for use with husky.
+branchPath=$(git symbolic-ref -q HEAD) #Somthing like refs/heads/myBranchName
+branchName=${branchPath##*/}      #Get text behind the last / of the branch path
+firstLine=$(head -n1 $HUSKY_GIT_PARAMS)
+if [ -z "$firstLine"  ] ;then #Check that this is not an amend by checking that the first line is empty
+    echo "Entre en sed"
+    sed -i "1s/^/($branchName): \n/" $HUSKY_GIT_PARAMS  #Insert branch name at the start of the commit message file
+fi
 
